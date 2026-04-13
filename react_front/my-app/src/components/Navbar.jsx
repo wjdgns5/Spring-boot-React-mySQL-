f29757f1f9ce4ad6c938a1b8e65f3e0d60de5c9f
wjdgns5 <80444098+wjdgns5@users.noreply.github.com>
2026-04-13 16:22:06 +0900

    로그아웃 처리

--- react_front/my-app/src/components/Navbar.jsx
+++ react_front/my-app/src/components/Navbar.jsx
@@ -1,11 +1,33 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 // 전역 상태(Redux state)에서 내가 필요한 값만 꺼내오는 기능
 import { useSelector } from "react-redux";
 
+import axiosInstance from "../api/axiosInstance";
+import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
+import { logout } from "../store/userSlice"; // Redux 에서 logout() 함수 불러옴
+
 const Navbar = () => {
   const location = useLocation();
   const { role, isAuthenticated } = useSelector((state) => state.user);
+
+  // Redux store에 액션(action)을 보내기 위한 함수
+  // action 이란? “이렇게 바꿔라” 라는 명령서
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axiosInstance.post("/api/auth/logout");
+    } catch (err) {
+      console.error("서버 로그아웃 실패: ", err);
+    }
+    Cookies.remove("accessToken");
+    dispatch(logout()); // 로그아웃 함수 실행?
+    navigate("/"); // 홈 페이지로 이동
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -25,6 +47,7 @@ const Navbar = () => {
                   관리자{" "}
                 </Link>
               )}
+              <Link onClick={handleLogout}>로그아웃</Link>
             </>
           ) : (
             <>
